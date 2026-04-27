4682f61d56b96f1454f6888256f1e66041c1ea48
Emirhanozcan16 <emirhanozcan900@gmail.com>
2026-04-27 13:08:57 +0300

    PWA eklendi

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'kalori-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
